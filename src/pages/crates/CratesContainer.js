98ea56cb258bc69dc9893bff1022116d46babd6d
antonicarol <antonicarolgit@gmail.com>
2022-01-29 16:48:14 +0100

    Added some frontend

--- src/pages/crates/CratesContainer.js
+++ src/pages/crates/CratesContainer.js
@@ -1,7 +1,67 @@
-import React from "react";
+import axios from "axios";
+import { formatEther, parseEther } from "ethers/lib/utils";
+import React, { useCallback, useEffect } from "react";
+import { useContractsContext } from "../../context/ContractProvider";
+import { actionTypes } from "../../context/reducer";
 import CratesSidebar from "./components/CratesSidebar";
 
+const fetchURI = async (item) => {
+  const tokenURI = item[2];
+  let result = [];
+  await axios.get(tokenURI).then((res) => {
+    if (res.status === 200) {
+      const { name, image } = res.data;
+      let _item = {
+        tokenId: parseInt(item[0].toHexString().toString(16)),
+        itemURI: tokenURI,
+        image: image.split("?")[0],
+        name: name,
+        price: formatEther(item[6]),
+        owner: item[4],
+      };
+      result = _item;
+    } else {
+      console.log("EII");
+    }
+  });
+  return result;
+};
+
 export default function CratesContainer() {
+  const [
+    { wallet, myItems, bellyERC721Contract, bellyERC20Contract },
+    dispatch,
+  ] = useContractsContext();
+  const fetchMyTokens = useCallback(async () => {
+    const _response = await bellyERC721Contract.getMyTokens({});
+    let formattedItems = [];
+    formattedItems = await Promise.all(
+      _response.map(async (item) => {
+        return await fetchURI(item);
+      })
+    );
+
+    const _balance = await bellyERC20Contract.balanceOf(wallet);
+    return {
+      myItems: formattedItems,
+      balance: formatEther(_balance),
+    };
+  }, [bellyERC20Contract, bellyERC721Contract, wallet]);
+
+  useEffect(() => {
+    if (wallet !== "") {
+      fetchMyTokens().then((res) => {
+        dispatch({
+          type: actionTypes.SET_MY_ITEMS,
+          myItems: res.myItems,
+          balance: res.balance,
+        });
+      });
+    }
+
+    return () => {};
+  }, [bellyERC721Contract.getItemsForSale, dispatch, fetchMyTokens, wallet]);
+
   return (
     <div className="flex flex-row">
       <CratesSidebar />
@@ -11,7 +71,33 @@ export default function CratesContainer() {
           style={{ maxHeight: "100vh" }}
         >
           <div className="w-full h-full relative">
-            <div className="w-full h-full relative"></div>
+            <div className="w-full h-full relative">
+              <div className="flex mt-8 flex-wrap justify-center w-full">
+
+                <div className="flex flex-col justidy-center  p-2 m-2 bg-[#282b39]">
+                  <img src="https://i.redd.it/udq9asephmpy.png"></img>
+  
+                  <div>
+                    <h4 className="uppercase text-[#a1a6b6]">Simple Crate</h4>
+                    <div className="flex justify-between align-center mt-2">
+                      <div className="">10 BLY</div>
+                      <button className="bg-[#6b7185] p-2 rounded-lg">Open</button>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col justidy-center  p-2 m-2 bg-[#282b39]">
+                  <img src="https://i.redd.it/udq9asephmpy.png"></img>
+  
+                  <div>
+                    <h4 className="uppercase text-[#a1a6b6]">Simple Crate</h4>
+                    <div className="flex justify-between align-center mt-2">
+                      <div className="">10 BLY</div>
+                      <button className="bg-[#6b7185] p-2 rounded-lg">Open</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/crates/components/CratesSidebar.js
+++ src/pages/crates/components/CratesSidebar.js
@@ -1,13 +1,41 @@
+import { Icon } from "@iconify/react";
 import React from "react";
+import { useContractsContext } from "../../../context/ContractProvider";
 
 export default function CratesSidebar() {
+  const [{ wallet, balance }] = useContractsContext();
   return (
     <div
-      className="hidden md:flex  sticky left-0 border-r "
+      className="hidden md:flex flex-col flex-none border-[#3a3f50] border-r"
       style={{ height: "94vh", width: "280px" }}
     >
-      <div className="pb-32 w-full">
-        <div className="w-full "></div>
+      <div className="w-full flex flex-col pt-6 px-12">
+        <div className="rounded w-full pt-5 pb-5 px-2 border border-[#3a3f50] text-center">
+          <div className="flex justify-center items-center">
+            <div className="flex items-center">
+              <h4 className="mr-4 text-lg font-medium">
+                {wallet.substring(0, 4)}...
+                {wallet.substring(wallet.length - 4)}
+              </h4>
+              <div className="hover:cursor-pointer">
+                <Icon icon="bx:bx-edit" color="white" />
+              </div>
+            </div>
+          </div>
+          <small className="block mt-4 mb-4 text-gray-2 font-medium truncate"></small>
+          <button className="px-2 py-2 relative rounded transition focus:outline-none border text-white border-gray-2 hover:border-[#a1a6b6] active:border-[#3a3f50] bg-gray-5 hover:bg-gray-4 active:bg-gray-6">
+            <span className="visible">
+              <div className="flex items-center">
+                <Icon icon="ri:copper-coin-fill" color="#cd7f32" />
+                <span className="ml-2">{balance} BLY</span>
+              </div>
+            </span>
+          </button>
+        </div>
+      </div>
+      <div className="flex-1 flex flex-col pt-2">
+       
+      
       </div>
     </div>
   );
