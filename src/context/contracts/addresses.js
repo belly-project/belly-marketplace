cb480592824d9d45895ac2f1b193df8ebfa06ca4
antonicarol <antonicarolgit@gmail.com>
2022-02-01 15:32:50 +0100

    Releas1

--- src/context/contracts/addresses.js
+++ src/context/contracts/addresses.js
@@ -1,5 +1,5 @@
 const bellyErc20 = "0x5e331E1e3033aAAbD55DC62d3B9e977d08175Af2";
-const bellyErc721 = "0x85d7357B6311410327b3255a7E57F4b6236e63D2";
-const bellyLoot = "0x78Bc98513fF00d525A871D542bD98DE71E59d3d0";
+const bellyErc721 = "0x494d48B4AF784d2945bBf2fE23fe617313D88717";
+const bellyLoot = "0x0Fac065A9c56e0F3842D99fA6BFEd3701611B734";
 
 export { bellyErc20, bellyErc721, bellyLoot };
