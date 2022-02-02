369cca2939e09fdbf397bd221a4a380ca66d0f49
antonicarol <antonicarolgit@gmail.com>
2022-02-02 17:26:34 +0100

    update

--- src/context/contracts/addresses.js
+++ src/context/contracts/addresses.js
@@ -1,6 +1,6 @@
 const bellyErc20 = "0x7B65Fe1945f6b985cD5Ef41C46a77bD3651d001a";
-const bellyErc721 = "0x52216E40a54df778223172D0a2528F42C21EDca5";
-const bellyLoot = "0xD2Ee33E33032960F7FF35F36294a1D65B1977523";
-const bellyChanceBid = "0x4F9e8222b3d9Dd28735170a327aEa4A25Eb398b0";
+const bellyErc721 = "0xF6B8D41B28E89954ea4ae033e7C581b351c0cB5A";
+const bellyLoot = "0xfC0635c008B743A190f678487d94Db87c553f544";
+const bellyChanceBid = "0xC325e4Cf93F7E51aC5E85C21Fcd9Cf3696f94515";
 
 export { bellyErc20, bellyErc721, bellyLoot, bellyChanceBid };
